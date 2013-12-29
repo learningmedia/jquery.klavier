470369c1731817ba98806d3b7c53d3d1763b3e99
Andreas Helmberger <public@ahelmberger.de>
2013-12-29 13:43:23 +0100

    Plugin structure is much simpler now.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function ($, undefined) {
   /*
     ======== A Handy Little QUnit Reference ========
     http://api.qunitjs.com/
@@ -20,29 +20,41 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery.klavier', {
-    // This will run before each test in this module.
+  module("jquery.klavier", {
     setup: function () {
       this.createContainers = function (count) {
-        var parent = $('#qunit-fixture'),
-            html = "";
-        for (var i = 0; i < count; i++) {
-          html += '<div></div>';
-        }
-        $(html).appendTo(parent);
-        return parent.children().last(count);
+        var html = new Array(count + 1).join("<div></div>");
+        return $("#qunit-fixture").html(html).children();
       };
     },
     teardown: function () {
-      $('#qunit-fixture').empty();
+      $("#qunit-fixture").empty();
     }
   });
 
-  test('is chainable', function () {
+  test("is chainable", function () {
     expect(1);
-    // Not a bad test to run on collection methods.
     var elems = this.createContainers(3);
-    strictEqual(elems.klavier(), elems, 'should be chainable');
+    strictEqual(elems.klavier(), elems);
   });
 
-}(jQuery));
+  test("instantiate on all elements", function () {
+    expect(3);
+    var elems = this.createContainers(3);
+    elems.klavier();
+    elems.each(function () {
+      notStrictEqual($(this).data("klavier"), undefined);
+    });
+  });
+
+  test("is destroyed on all elements", function () {
+    expect(3);
+    var elems = this.createContainers(3);
+    elems.klavier();
+    elems.klavier("destroy");
+    elems.each(function () {
+      strictEqual($(this).data("klavier"), undefined);
+    });
+  });
+
+}) (jQuery);
