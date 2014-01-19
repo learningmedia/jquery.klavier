ecc9c58a992595b5840662a41ac1d64d96f1b288
Andreas Helmberger <public@ahelmberger.de>
2014-01-19 22:46:06 +0100

    Remove old QUnit tests and references.

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,13 +36,6 @@ module.exports = function (grunt) {
         dest: 'dist/<%= pkg.name %>.min.js'
       }
     },
-    qunit: {
-      all: {
-        options: {
-          urls: ['http://localhost:9000/test/test.html']
-        }
-      }
-    },
     karma: {
       options: {
         configFile: 'karma.conf.js',
@@ -98,7 +91,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
